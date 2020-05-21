d5df38053f2c5506d2b3e55048be9de6dfa9540f
Jesus Bautista <jesbam98@gmail.com>
2020-05-21 17:49:19 -0500

    Added hapi setup

---
+++ settings.js
@@ -0,0 +1,21 @@
+
+module.exports = {
+    port: process.env.PORT || 3000,
+    env: process.env.ENV || 'development',
+
+    // Enviroment-depending settings
+    development: {
+        db: {
+            dialect: 'sqlite',
+            storage: ':memory',
+            operatorsAliases: false,
+        },
+    },
+    production: {
+        db: {
+            dialect: 'sqlite',
+            storage: 'db/database.sqlite',
+            operatorsAliases: false,
+        }
+    }
+};
